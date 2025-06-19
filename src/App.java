079434fe6224d3237d7feab7fbc3ef1101795903
MerelynSoza <merelyn.soza555@std.uni.edu.ni>
2025-06-18 19:36:13 -0600

    primer cambio confirmado

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String nombre = "Estudiante";
+        int a=5;
+        int b=3;
+        
+
+        int suma= a + b;
+        System.out.println("hola"+ nombre + "!");
+        System.out.println("la suma de" + a + "+"  + b +"es:"+suma);
+    }   
+}
