577a792bcae5805dc060ef71fe80962f911e8458
MerelynSoza <merelyn.soza555@std.uni.edu.ni>
2025-06-18 20:31:32 -0600

    tercer cambio confirmado

--- src/App.java
+++ src/App.java
@@ -4,10 +4,11 @@ public class App {
         String nombre = "Estudiante";
         int a=34;
         int b=14;
-        
+        int multiplicacion = a*b;
 
         int suma= a + b;
         System.out.println("hola"+ nombre + "!");
         System.out.println("la suma de" + a + "+"  + b +"es:"+suma);
+        System.out.println("la multimplicacion de"+a+ "x" +b+ "es = "+multiplicacion);
     }   
 }
